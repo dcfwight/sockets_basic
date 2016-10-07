31947eb686ac4c0ed4099ed649b177403955cec6
Doug Wight <dcfwight@gmail.com>
2016-10-07 17:28:31 +0100

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,9 @@ var socket = io(); <!--this is a function defined when we loaded in the io libra
 socket.on('connect', function() {
     console.log('connected to socket.io server');
     })
+
+// this listens to the specific event that you named in server.js (remember, it can be named anything.)
+socket.on('message', function(message){
+    console.log('New message');
+    console.log(message.text);
+    })
--- server.js
+++ server.js
@@ -7,8 +7,22 @@ var io = require('socket.io')(http); // this is the format that socket expects -
 
 app.use(express.static(__dirname + '/public')) // tells where the server should look for files.
 
-io.on('connection', function(){
-    console.log('user connected via socket.io');    
+// this is a listener, which listens out for an individual client connecting to the server. socket in the function is the individual connection
+io.on('connection', function(socket){
+    console.log('user connected via socket.io');
+    
+    // this makes the server listen for socket emitters - in this case message, so it can emit something to all other connections.
+    socket.on('message', function(message) {
+        console.log('Message received: '+ message.text);
+        socket.broadcast.emit('message', message); // this broadcasts it to everybody BUT the sender. if you wanted to include the sender too, use io.emit
+    
+    })
+    
+    // emit takes two arguments - first is the event name, which can be anything you want. Second argument is whatever data you want to emit.
+    // in this case it is an object, as you can store lots of different things.
+    socket.emit('message',{
+            text: 'Welcome to the chat application!'
+        } )
 }); // this allows you to listen for events. First argument is the name of the event.
 
 http.listen(PORT, function(){
